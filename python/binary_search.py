d4ab1696323d008d0dd6f667f5c56c78ca9e52a4
Siavash Aghili <siavashaghili@gmail.com>
2023-05-28 18:37:45 +0100

    add new solution

--- python/binary_search.py
+++ python/binary_search.py
@@ -1,4 +1,9 @@
 # A sorted array and an item
+
+
+from typing import List
+
+
 def binary_search_ver_one(thelist, item):
     low = 0
     high = len(thelist) - 1
@@ -28,21 +33,33 @@ def binary_search_ver_two(data, target, low, high):
             return binary_search_ver_two(data, target, mid + 1, high)
 
 
-def binary_search_ver_three(haystack, needle) -> int:
-    lo = 0
-    hi = len(haystack)
 
-    while lo <= hi:
-        mid = lo + (hi - lo) // 2
-        guess = haystack[mid]
-        if guess == needle:
-            return mid
-        elif guess > needle:
-            hi = mid
-        else:
-            lo = mid + 1
-    return False
+
+##Given an array of integers nums which is sorted in ascending order, 
+# and an integer target, write a function to search target in nums. 
+# If target exists, then return its index. Otherwise, return -1.
+
+#You must write an algorithm with O(log n) runtime complexity.
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return -1
 
 
 mylist = [1, 3, 5, 7, 9, 11, 13, 15, 17]
-print(binary_search_ver_three(mylist, 7))
+obj = Solution()
+print(obj.search(mylist, 7))
+
